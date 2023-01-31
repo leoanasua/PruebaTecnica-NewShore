36d83252e0a8b2b5e3349a135f38448fa5f3f5b5
leoanasua <leoana.suarez@tekus.co>
2023-01-31 13:31:39 -0500

    Fix unitest

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -1,4 +1,13 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { FavoriteDestinationsCardsComponent } from './components/favorite-destinations-cards/favorite-destinations-cards.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +17,26 @@ describe('BookingFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent,
+        HeaderComponent,
+        FavoriteDestinationsCardsComponent],
+      imports: [HttpClientTestingModule,
+        MatDialogModule,
+        MatRadioModule,
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule,
+        MatRadioModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        }
+      ]
     })
       .compileComponents();
 
