6f19883deb8c093f77ed61b3b13f48a688794df5
leoanasua <leoana.suarez@tekus.co>
2023-01-31 02:39:52 -0500

    Add module

--- src/app/modules/flights/flights.module.ts
+++ src/app/modules/flights/flights.module.ts
@@ -1,26 +1,40 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FlightsComponent } from './flights.component';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FlightsListComponent } from './components/flights-list/flights-list.component';
+import { JourneyCardModule } from './components/journey-card/journey-card.module';
+import { JourneyDetailsComponent } from './components/journey-details/journey-details.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FlightsComponent
+    component: FlightsListComponent
   }
 ];
 
 @NgModule({
   declarations: [
-    FlightsComponent
+    FlightsListComponent,
+    JourneyDetailsComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MatIconModule,
     SharedModule,
+    MatButtonToggleModule,
+    FormsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    JourneyCardModule
+  ],
+  exports: [
+    JourneyDetailsComponent
   ]
 })
 export class FlightsModule { }
